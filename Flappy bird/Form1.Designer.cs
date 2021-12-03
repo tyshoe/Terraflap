4e8a84beae527af6a58d0e70a1a93c9bb6066e79
Tyler Shoemake <w977584@usm.edu>
2021-12-02 22:42:08 -0600

    Add project files.

---
+++ Flappy bird/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Flappy_bird
+{
+    partial class Game_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bird_pic = new System.Windows.Forms.PictureBox();
+            this.Top_pipe_pic = new System.Windows.Forms.PictureBox();
+            this.Bot_pipe_pic = new System.Windows.Forms.PictureBox();
+            this.Ground_pic = new System.Windows.Forms.PictureBox();
+            this.score_lbl = new System.Windows.Forms.Label();
+            this.game_timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bird_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Top_pipe_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bot_pipe_pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ground_pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bird_pic
+            // 
+            this.bird_pic.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bird_pic.BackColor = System.Drawing.Color.Transparent;
+            this.bird_pic.Image = global::Flappy_bird.Properties.Resources.bird;
+            this.bird_pic.Location = new System.Drawing.Point(282, 262);
+            this.bird_pic.Margin = new System.Windows.Forms.Padding(1);
+            this.bird_pic.Name = "bird_pic";
+            this.bird_pic.Size = new System.Drawing.Size(80, 60);
+            this.bird_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.bird_pic.TabIndex = 0;
+            this.bird_pic.TabStop = false;
+            // 
+            // Top_pipe_pic
+            // 
+            this.Top_pipe_pic.BackColor = System.Drawing.Color.Transparent;
+            this.Top_pipe_pic.Image = global::Flappy_bird.Properties.Resources.pipedown;
+            this.Top_pipe_pic.Location = new System.Drawing.Point(582, 0);
+            this.Top_pipe_pic.Name = "Top_pipe_pic";
+            this.Top_pipe_pic.Size = new System.Drawing.Size(100, 218);
+            this.Top_pipe_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Top_pipe_pic.TabIndex = 1;
+            this.Top_pipe_pic.TabStop = false;
+            // 
+            // Bot_pipe_pic
+            // 
+            this.Bot_pipe_pic.Image = global::Flappy_bird.Properties.Resources.pipe;
+            this.Bot_pipe_pic.Location = new System.Drawing.Point(582, 359);
+            this.Bot_pipe_pic.Name = "Bot_pipe_pic";
+            this.Bot_pipe_pic.Size = new System.Drawing.Size(100, 226);
+            this.Bot_pipe_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Bot_pipe_pic.TabIndex = 2;
+            this.Bot_pipe_pic.TabStop = false;
+            // 
+            // Ground_pic
+            // 
+            this.Ground_pic.Image = global::Flappy_bird.Properties.Resources.ground;
+            this.Ground_pic.Location = new System.Drawing.Point(0, 582);
+            this.Ground_pic.Name = "Ground_pic";
+            this.Ground_pic.Size = new System.Drawing.Size(682, 92);
+            this.Ground_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ground_pic.TabIndex = 3;
+            this.Ground_pic.TabStop = false;
+            // 
+            // score_lbl
+            // 
+            this.score_lbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.score_lbl.AutoSize = true;
+            this.score_lbl.BackColor = System.Drawing.Color.Transparent;
+            this.score_lbl.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.score_lbl.ForeColor = System.Drawing.Color.Black;
+            this.score_lbl.Image = global::Flappy_bird.Properties.Resources.ground;
+            this.score_lbl.Location = new System.Drawing.Point(310, 605);
+            this.score_lbl.Name = "score_lbl";
+            this.score_lbl.Size = new System.Drawing.Size(52, 60);
+            this.score_lbl.TabIndex = 4;
+            this.score_lbl.Text = "0";
+            // 
+            // game_timer
+            // 
+            this.game_timer.Enabled = true;
+            this.game_timer.Interval = 20;
+            this.game_timer.Tick += new System.EventHandler(this.gameTimerEvent);
+            // 
+            // Game_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(682, 674);
+            this.Controls.Add(this.score_lbl);
+            this.Controls.Add(this.Ground_pic);
+            this.Controls.Add(this.Bot_pipe_pic);
+            this.Controls.Add(this.Top_pipe_pic);
+            this.Controls.Add(this.bird_pic);
+            this.Name = "Game_Form";
+            this.Text = "Flappy Bird";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gamekeyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gamekeyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.bird_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Top_pipe_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bot_pipe_pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ground_pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox bird_pic;
+        private System.Windows.Forms.PictureBox Top_pipe_pic;
+        private System.Windows.Forms.PictureBox Bot_pipe_pic;
+        private System.Windows.Forms.PictureBox Ground_pic;
+        private System.Windows.Forms.Label score_lbl;
+        private System.Windows.Forms.Timer game_timer;
+    }
+}
+
---
+++ Flappy bird/Form1.cs
@@ -0,0 +1,174 @@
+﻿//ideas
+//add highscore with text file
+//add leaderboards screen
+//add different modes
+//increase diffulty with playtime?
+//difficulty settings
+//add more sounds for death and flap
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Flappy_bird
+{
+    public partial class Game_Form : Form
+    {
+        private int _pipeSpeed = 10;
+        private int _gravity = 8;
+        private int _score = 0;
+
+
+        public Game_Form()
+        {
+            InitializeComponent();
+            Bot_pipe_pic.Left = 700;
+            Top_pipe_pic.Left = 700;
+
+        }
+
+        private void gameTimerEvent(object sender, EventArgs e)
+        {
+            bird_pic.Top += _gravity;
+            Bot_pipe_pic.Left -= _pipeSpeed;
+            Top_pipe_pic.Left -= _pipeSpeed;
+            score_lbl.Text = _score.ToString();
+
+            
+            //if left side pipe hits coor 240, add point
+            if (Bot_pipe_pic.Left == 240 || Top_pipe_pic.Left == 240)
+            {
+                _score++;
+                gain_point();
+            }
+
+
+
+            //when pipe goes off screen, change pipe to start over again
+            if (Bot_pipe_pic.Left < -100 || Top_pipe_pic.Left < -100)
+            {
+                pipe_location();
+            }
+
+
+
+            //if bird hits pipes or ground
+            if (bird_pic.Bounds.IntersectsWith(Bot_pipe_pic.Bounds) || bird_pic.Bounds.IntersectsWith(Top_pipe_pic.Bounds) || bird_pic.Bounds.IntersectsWith(Ground_pic.Bounds))
+            {
+                _ = endGameAsync();
+            }
+
+        }
+
+        //method to set sound to play the point gain sound
+        private void gain_point() // defining the function
+        {
+            SoundPlayer audio = new SoundPlayer(Properties.Resources.point); // here WindowsFormsApplication1 is the namespace and Connect is the audio file name
+            audio.Play();
+        }
+
+        private void pipe_location()
+        {
+            System.Random random = new System.Random();
+            int number = random.Next(1,4);
+
+            if (number == 1)//top location
+            {
+                Top_pipe_pic.Top = -125;
+                Top_pipe_pic.Left = 700;
+                Top_pipe_pic.Height = 218;
+
+
+                Bot_pipe_pic.Top = 250;
+                Bot_pipe_pic.Left = 700;
+                Bot_pipe_pic.Height = 351;
+
+            }
+            if (number == 2)//middle location
+            {
+                Top_pipe_pic.Top = 0;
+                Top_pipe_pic.Left = 700;
+                Top_pipe_pic.Height = 218;
+
+                Bot_pipe_pic.Top = 375;
+                Bot_pipe_pic.Left = 700;
+                Bot_pipe_pic.Height = 226;
+
+            }
+            if (number == 3)//bottom location
+            {
+                Top_pipe_pic.Top = 0;
+                Top_pipe_pic.Left = 700;
+                Top_pipe_pic.Height = 343;
+
+                Bot_pipe_pic.Top = 500;
+                Bot_pipe_pic.Left = 700;
+                Bot_pipe_pic.Height = 226;
+            }
+
+
+        }
+
+        private void gamekeyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                _gravity = -8;
+            }
+        }
+
+        private void gamekeyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                _gravity = 8;
+            }
+        }
+
+        private void reset_game()
+        {
+            _score = 0;
+            Bot_pipe_pic.Left = 700;
+            Top_pipe_pic.Left = 700;
+            bird_pic.Location = new Point(268, 268);
+            _pipeSpeed = 0;
+            _gravity = 0;
+
+        }
+
+        private void start_game()
+        {
+            _pipeSpeed = 10;
+            _gravity = 5;
+            game_timer.Start();
+        }
+
+        private async Task endGameAsync()
+        {
+            game_timer.Stop();//stops game
+
+            //message box
+            DialogResult d;
+            d = MessageBox.Show("Play Again?", "Game Over!", MessageBoxButtons.YesNo);
+
+            if (d == DialogResult.No)//if they want to close game
+            {
+                Close();
+            }
+            if (d == DialogResult.Yes)//if they want to continue
+            {
+                reset_game();
+                await Task.Delay(750);
+                start_game();
+                 
+            }
+        }
+    }
+}
